634d4877e1245c4d13bfd3e6379cf63100a8f84e
afshin <afshin@email.com>
2022-09-10 18:32:28 +0430

    ADD Custom User Model

--- LoginGmail/accounts/admin.py
+++ LoginGmail/accounts/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
+from django.contrib.auth.models import Group
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    model = CustomUser
+    list_display = ('email', 'phone_number')
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('phone_number', )}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('phone_number', )}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.unregister(Group)
+
